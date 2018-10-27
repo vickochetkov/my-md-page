0fac8f0a1e1726f0ad5198b331b0742db75a59b0
Victor <vickochetkov@gmail.com>
2018-10-27 11:54:12 -0700

    rebuild on marked.js

--- src/components/initial-text.js
+++ src/components/initial-text.js
@@ -1,3 +1,48 @@
-const initialText = "# Header\nWelcome to md-page, the easiest way to make a webpage from just markdown...\n## header two\n### header three\n#### header four\n##### header five\n*this will be italic*\n**this will be bold**\n~~this will be strike through~~\n__this will be underlined__\n## Lists\n### Unordered\n    * item one\n    * item two\n       * item a\n       * item b\n### Ordered\n    1. Item 1\n    1. Item 2\n    1. Item 3\n       1. item 3a\n\n# links\n[md-page Github repo](https://github.com/oscarmorrison/md-page) *give us a :star:*\n\n[demo page](https://oscarmorrison.com/md-page/)\n\nThanks for stopping by!"
+const initialText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+>      Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
 
 export default initialText;
--- src/components/previewer.js
+++ src/components/previewer.js
@@ -1,27 +1,44 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {  MarkdownPreview  } from 'react-marked-markdown';
+// import {  MarkdownPreview  } from 'react-marked-markdown';
+import marked from 'marked'
 
 export default class Previewer extends Component {
+
+
+  mdText({text}) {
+    var markup = marked(text,
+      {
+        sanitize: true,
+        breaks: true,
+        tables: true
+      });
+    return { __html: markup };
+  }
+
   // console.log({text});
   render() {
     const { text } = this.props;
     return (
-      <div id="preview" >
-        <MarkdownPreview
-        value={ text }
-        markedOptions={{
-          breaks: true,
-          smartypants: true,
-          tables: true,
-          langPrefix: 'langClass',
-          highlight: function(code) {
-            return require('highlight.js').highlightAuto(code).value;
-          }
-        }}
-        />
-      </div>
+      <div id="preview" dangerouslySetInnerHTML={this.mdText({ text })} />
     )
+
+    // (
+    //   <div id="preview" >
+    //     <MarkdownPreview
+    //     value={ text }
+    //     markedOptions={{
+    //       breaks: true,
+    //       smartypants: true,
+    //       tables: true,
+    //       langPrefix: 'langClass',
+    //       highlight: function(code) {
+    //         return require('highlight.js').highlightAuto(code).value;
+    //       }
+    //     }}
+    //     />
+    //   </div>
+    // )
   }
 }
 
